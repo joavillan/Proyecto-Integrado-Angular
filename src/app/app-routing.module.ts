99b4232c5fc221f85f514b5e7e05f12412de0e1b
Joaquin Villegas <joa.villan@outlook.com>
2020-03-19 14:26:24 +0100

    inicio del desarrollo de angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HeaderComponent } from './Components/header/header.component';
+import { HomeComponent } from './Components/home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'Home', component: HomeComponent
+},
+{path: '', redirectTo: '/Home',pathMatch: 'full'},
+{
+  path        : '**',
+  pathMatch   : 'full',
+  component   : HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
