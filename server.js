de1a8386fac9c6951321792ce363385f25c5ae77
joavillan <45419691+joavillan@users.noreply.github.com>
2020-05-26 14:04:41 +0200

    aaa

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/dist/proyecto-integrado-angular'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/proyecto-integrado-angular/index.html'));
+res.sendFile(path.join(__dirname+'/dist/Proyecto-Integrado-Angular/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
